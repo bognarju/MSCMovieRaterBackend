beb438f7498d9f426e32685eb79b39e2415be5d6
VZsolt <VZsolt@LHU0-L92>
2017-03-23 12:28:51 +0100

    Add project files.

---
+++ MSCMovieRaterBackend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MSCMovieRaterBackend.Startup))]
+
+namespace MSCMovieRaterBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
